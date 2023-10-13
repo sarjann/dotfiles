3cce2378a89da062a402859836a88fca7a0c08d1
Sarjan Narwan <imsarj@gmail.com>
2023-10-13 17:40:43 +0100

    Additional stuff

--- setup.sh
+++ setup.sh
@@ -3,8 +3,14 @@ set -e
 
 
 function _general {
-    sudo apt install git, libsdl2-gfx-dev, curl, apt-transport-https \
-        ca-certificates, gnupg, lsb-release -y
+    sudo apt install git libsdl2-gfx-dev curl apt-transport-https \
+        ca-certificates gnupg lsb-release build-essential zlib1g-dev \
+        libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev \
+        libffi-dev wget tmux liblzma-dev bzip2 sqlite libsqlite3-dev \
+        libbz2-dev python-tk python3-tk python3-dotenv-cli tk-dev git-lfs jq -y
+    sudo apt install apt-transport-https fuse libfuse -y
+    sudo apt install xclip ffmpeg ripgrep -y
+    sudo apt install postgresql redis rabbitmq-server -y
 }
 
 function _docker {
@@ -12,8 +18,8 @@ function _docker {
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
         https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
         | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+    sudo apt update
+    sudo apt install docker-ce docker-ce-cli containerd.io -y
 }
 
 function _app_tools {
@@ -24,6 +30,8 @@ function _app_tools {
     sudo snap install spotify
     sudo snap install code --classic
     sudo snap install vlc
+    sudo snap install nvim --classic
+    sudo snap install kubectl --classic
 }
 
 function _regolith {
@@ -33,13 +41,18 @@ function _regolith {
         https://regolith-desktop.org/release-3_0-ubuntu-jammy-amd64 jammy main" | \
         sudo tee /etc/apt/sources.list.d/regolith.list
 
-    sudo apt install regolith-desktop regolith-session-sway regolith-look-nord -y
     sudo apt update -y
+    sudo apt install regolith-desktop regolith-session-sway regolith-look-nord -y
     sudo apt install regolith-desktop regolith-session-flashback regolith-look-lascaille -y
 }
 
 function _neovim {
     sudo apt install neovim -y
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    mkdir -p ~/.config/nvim/pack/airblade/start
+    cd ~/.config/nvim/pack/airblade/start
+    git clone https://github.com/airblade/vim-gitgutter.git
 }
 
 function _yubikey {
@@ -49,24 +62,34 @@ function _yubikey {
 }
 
 function _go {
-    sudo apt install golang-go -y
+    sudo snap install go --classic
+    # Dot env from go
+    go install github.com/rhysd/dotfiles@latest
 }
 
 function _rust {
     curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+    source ~/.bashrc
     rustup update
+    cargo install cargo-watch
 }
 
-function _pyenv {
+function _python {
     git clone https://github.com/pyenv/pyenv.git ~/.pyenv
     cd ~/.pyenv && src/configure && make -C src
     echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
     echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+    source ~/.bashrc
     echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
     source ~/.bashrc
     git clone https://github.com/alefpereira/pyenv-pyright.git $(pyenv root)/plugins/pyenv-pyright
+    source ~/.bashrc
     pyenv install 3.10.12
+
+    curl -sSL https://install.python-poetry.org | python3 -
 }
 
 function _node {
@@ -101,7 +124,7 @@ _neovim
 _yubikey
 _go
 _rust
-_pyenv
+_python
 _node
 _java
 _ocaml
